b509efb5bd7463099d403fe374552b0a6af024f6
richie <rishikalpadas@outlook.com>
2025-04-19 17:37:00 +0530

    don't know what changed but it is what it is

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import Card from "./components/Card.jsx";
 import Counter from "./components/Counter.jsx";
 
 function App() {
-  
-
   return (
     <>
-      <div className="heading bg-amber-300 h-30 flex flex-col items-center justify-center">
-
-      <h1 className="bg-green-500 text-black p-4 rounded-2xl font-bold border-2 border-gray-600">Hello World!</h1>
+      <div className="heading bg-amber-300 h-30 flex flex-col items-center justify-center p-2 mb-3">
+        <h1 className="bg-green-500 text-black p-4 rounded-2xl font-bold border-2 border-gray-600">
+          Hello World!
+        </h1>
       </div>
-      {/* <Counter /> */}
+      <Counter />
       <div className="flex gap-4 flex-wrap justify-center">
-
-      <Card avatar="https://images.pexels.com/photos/7704090/pexels-photo-7704090.jpeg?auto=compress&cs=tinysrgb&w=600" role="Software Developer" name="Rishikalpa Das" profile="Passionate software developer, coder, gamer, music enthusiast"/>
-      <Card avatar="https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=600" role=".NET Developer" name="Payal Adhikary" profile="Sweet, caring, and kind person, loves to learn new things"/>
+        <Card
+          avatar="https://images.pexels.com/photos/7704090/pexels-photo-7704090.jpeg?auto=compress&cs=tinysrgb&w=600"
+          role="Software Developer"
+          name="Rishikalpa Das"
+          profile="Passionate software developer, coder, gamer, music enthusiast"
+        />
+        <Card
+          avatar="https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=600"
+          role=".NET Developer"
+          name="Payal Adhikary"
+          profile="Sweet, caring, and kind person, loves to learn new things"
+        />
       </div>
-
     </>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,29 +1,34 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const Counter = () => {
   const [myCounter, setCounter] = useState(0);
   return (
     <>
-      <button
-        onClick={() => {
-          if (myCounter > 0) {
-            setCounter(myCounter - 1);
-          } else {
-            alert("Counter cannot be less than 0");
-          }
-        }}
-      >
-        -
-      </button>
+      <div className="flex gap-2 justify-evenly items-center mb-3">
+        <button
+          onClick={() => {
+            if (myCounter > 0) {
+              setCounter(myCounter - 1);
+            } else {
+              alert("Counter cannot be less than 0");
+            }
+          }}
+        >
+          -
+        </button>
 
-      <h1>Counter: {myCounter}</h1>
-      <button
-        onClick={() => {
-          setCounter(myCounter + 1);
-        }}
-      >
-        +
-      </button>
+        <h2 className="text-3xl">Counter: {myCounter}</h2>
+        <button
+          onClick={() => {
+            setCounter(myCounter => myCounter + 1);
+            setCounter(myCounter => myCounter + 1);
+            setCounter(myCounter => myCounter + 1);
+            setCounter(myCounter => myCounter + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
 };
